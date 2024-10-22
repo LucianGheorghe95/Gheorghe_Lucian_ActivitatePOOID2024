89c14c702d14dc4a26bdb9b38ac278c23da2dadf
LucianGheorghe95 <gheorghe.lucian1995@yahoo.com>
2024-10-22 20:51:03 +0300

    learning

--- SeminarPO/seminar.cpp
+++ SeminarPO/seminar.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main() {
-	cout << "hello" << endl;
-	cout << "test" << endl;
-	int x = 30;
-	cout << x;
+	int x, y, z;
+	x = y = z = 30;
+
+	const int a = 30;
+	const string cnp = "12324343";
+	const int id = 1;
+	const string nume = "lucian";
+
+	cout << cnp << endl;
+
+	int studentID = 15;
+	int studentAge = 22;
+	float studentFee = 3500.20;
+	char studentGrade = 'b';
+
+	cout << studentID << endl;
+	cout << studentAge << endl;
+	cout << studentFee << endl;
+	cout << studentGrade;
+
+
+	return 0;
 }
